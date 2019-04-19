1ad0b70625564ab4fc4c5d9b7cf36a3f3df293f9
poberWong <poberwong@gmail.com>
2019-04-19 20:11:22 +0800

    refactor(local data): change location of local data
    move local data from multi files to single file

--- src/helpers/data.js
+++ src/helpers/data.js
@@ -3,7 +3,14 @@
  * Created by poberwong on 2019-04-19.
  */
 
-export default [
+export const profile = {
+  'profile-image': 'http://img2.findthebest.com/sites/default/files/688/media/images/Mingle_159902_i0.png',
+  'avatar': 'http://info.thoughtworks.com/rs/thoughtworks2/images/glyph_badge.png',
+  'nick': 'John Smith',
+  'username': 'jsmith'
+}
+
+export const tweets = [
   {
     'content': '沙发！',
     'images': [
