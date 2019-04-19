7275c683677b9bf80dbd184a3b7571f839a47375
poberWong <poberwong@gmail.com>
2019-04-19 18:46:44 +0800

    chore(source of data): put data locally
    put data locally so that we can load data without request.

---
+++ src/helpers/data/profile.js
@@ -0,0 +1,10 @@
+/**
+ * Created by poberwong on 2019-04-19.
+ */
+
+export default {
+  'profile-image': 'http://img2.findthebest.com/sites/default/files/688/media/images/Mingle_159902_i0.png',
+  'avatar': 'http://info.thoughtworks.com/rs/thoughtworks2/images/glyph_badge.png',
+  'nick': 'John Smith',
+  'username': 'jsmith'
+}
