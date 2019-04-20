1afc3f5c878e705f7cb1a157a65893d9cf68fac9
poberWong <poberwong@gmail.com>
2019-04-20 19:20:38 +0800

    feat(Image): rewrite Image.js
    use Custom Image which has default image while loading

---
+++ src/components/Image.js
@@ -0,0 +1,50 @@
+/**
+ * Created by poberwong on 2019-04-20.
+ */
+
+import React from 'react'
+import { Image, ImageBackground } from 'react-native'
+import FastImage from 'react-native-fast-image'
+
+export default class extends React.Component {
+  static propTypes = Image.propTypes
+  static defaultProps = {
+    defaultSource: require('../assets/default_img.png')
+  }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      defaultWidth: SCREEN_WIDTH / 3,
+      defaultHeight: SCREEN_WIDTH / 3
+    }
+  }
+
+  _onLoad = ({nativeEvent: {width, height}}) => {
+    this.setState({
+      isLoading: false,
+      defaultWidth: width,
+      defaultHeight: height
+    })
+  }
+
+  render () {
+    const {isLoading, defaultHeight, defaultWidth} = this.state
+    const {style = {}, source, defaultSource, ...otherProps} = this.props
+    style.height = style.height || defaultHeight
+    style.width = style.width || defaultWidth
+
+    if (isLoading) {
+      return (
+        <ImageBackground style={[style, {backgroundColor: 'transparent'}]} source={defaultSource} {...otherProps}>
+          <FastImage style={{flex: 1}} source={source} onLoad={this._onLoad} />
+        </ImageBackground>
+      )
+    } else {
+      return (
+        <FastImage {...this.props} />
+      )
+    }
+  }
+}
